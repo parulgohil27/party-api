78b8551edc45357625f0482c6745a4ae81579826
tanwar_pankaj_itech <tanwar.pankaj.itech@gmail.com>
2024-05-31 19:52:22 +0000

    updated username

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
-    user: "DB_USERNAME",
-    password: "DB_PASSWORD",
-    host: "DB_HOST",
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
     port: 3306,
-    database: "DB_NAME"
+    database: process.env.DB_NAME
 })
 
 module.exports = pool;
